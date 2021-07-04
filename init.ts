683d51b9c80200f0780d0505c7d26a86f4492ca7
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-07-04 12:53:08 +0200

    feat: use cliffy to build the CLI

--- init.ts
+++ init.ts
@@ -1,4 +1,4 @@
-import { args } from './parser.ts';
+import { Command, EnumType } from "./deps.ts";
 import { writeFileOrWarn, mkdirOrWarn, hasFileExtension } from './utils.ts';
 import { vsCodeDebugConfig } from './configs/debugconfig_vscode.ts';
 
@@ -19,67 +19,91 @@ const defaults = {
     settings: encoder.encode(`{\n\t"deno.enable": true\n}`),
     settingsDir: ".vscode",
     settingsFile: "settings.json",
-};
+};  
 
-async function addContents(entrypoint?: string, depsEntrypoint?: string) {  
-    await writeFileOrWarn(entrypoint ?? defaults.entrypoint, defaults.module);
-    await writeFileOrWarn(depsEntrypoint ?? defaults.depsEntrypoint, defaults.depsModule);
-    await writeFileOrWarn(defaults.gitignore, defaults.gitignorePatterns);
+const choices = new EnumType(["oak", "restful_oak"]);   // aleph, opine
 
-    await mkdirOrWarn(defaults.settingsDir);
+await new Command()
+    .type("choices", choices)
+    .name("deno-init")
+    .version("0.5.3")
+    .description("Start a new Deno project with a single command")
+    .option("-f, --force [force:boolean]", "Force overwrite of existing files/directories. Helpful to re-initialize but use with caution!")
+    .option("-n, --name [name:string]", "Create the project in a new directory.")
+    .option<{ choices: typeof choices }>(
+        "-t, --template [template:string]",
+        "Initialize the project with a template. Options: oak, restful_oak"
+    )
+    .option("-y, --yes [yes:boolean]", "Answer with 'y' to all prompts")
+    .action(async ({ force, name, template, yes }: any) => {  
+        if (yes === true) {
+            if (name) {
+                await mkdirOrWarn(name, force);
+                Deno.chdir(name);
+            }
+            await fetchTemplate(template);
+            await addContents(undefined, undefined, force);
+        } 
+        else {
+            const ts = prompt("TypeScript? (y/n)", 'y');
+            const isTypeScript = (ts === 'y' || ts === 'Y');
+        
+            if (!isTypeScript && template) {
+                console.warn("Warning: Selected JavaScript with a TypeScript template.");
+            }
+        
+            const extension = isTypeScript ? defaults.extension : "js";
+        
+            let entrypoint = <string> prompt(`Entrypoint:`, `mod.${extension}`);
+        
+            if (!hasFileExtension(entrypoint, extension)) {
+                entrypoint = `${entrypoint}.${extension}`;
+            }
+        
+            let depsEntrypoint = <string> prompt("Dependency entrypoint", `deps.${extension}`);
+        
+            if (!hasFileExtension(depsEntrypoint, extension)) {
+                depsEntrypoint = `${depsEntrypoint}.${extension}`;
+            }
+        
+            const addDebug = <string> prompt("Add debug configuration? (y/n)", defaults.debug);
+            
+            if (addDebug !== 'y' && addDebug !== 'Y') {
+                defaults.debug = addDebug;
+            }
+
+            if (name) {
+                await mkdirOrWarn(name, force);
+                Deno.chdir(name);
+            }
+        
+            await fetchTemplate(template);
+            await addContents(entrypoint, depsEntrypoint, force);
+        }
+    })
+    .parse(Deno.args);
+
+
+async function addContents(entrypoint?: string, depsEntrypoint?: string, force: boolean = false) {  
+    await writeFileOrWarn(entrypoint ?? defaults.entrypoint, defaults.module, force);
+
+    await writeFileOrWarn(depsEntrypoint ?? defaults.depsEntrypoint, defaults.depsModule, force);
+
+    await writeFileOrWarn(defaults.gitignore, defaults.gitignorePatterns, force);
+
+    await mkdirOrWarn(defaults.settingsDir, force);
     Deno.chdir(defaults.settingsDir);
 
-    await writeFileOrWarn(defaults.settingsFile, defaults.settings);
+    await writeFileOrWarn(defaults.settingsFile, defaults.settings, force);
     
     if (defaults.debug === 'y' || defaults.debug === 'Y') {
-        await writeFileOrWarn(defaults.debugFile, defaults.debugConfig);
+        await writeFileOrWarn(defaults.debugFile, defaults.debugConfig, force);
     }
 }
 
-if (args.name) {
-    await mkdirOrWarn(args.name);
-    Deno.chdir(args.name);
-}
-
-if (args.yes === true) {
-    await checkTemplateArg();
-    await addContents();
-} 
-else {
-    const ts = prompt("TypeScript? (y/n)", 'y');
-    const isTypeScript = (ts === 'y' || ts === 'Y');
-
-    if (!isTypeScript && args.template) {
-        console.warn("Warning: Selected JavaScript with a TypeScript template.");
-    }
-
-    const extension = isTypeScript ? defaults.extension : "js";
-
-    let entrypoint = <string> prompt(`Entrypoint:`, `mod.${extension}`);
-
-    if (!hasFileExtension(entrypoint, extension)) {
-        entrypoint = `${entrypoint}.${extension}`;
-    }
-
-    let depsEntrypoint = <string> prompt("Dependency entrypoint", `deps.${extension}`);
-
-    if (!hasFileExtension(depsEntrypoint, extension)) {
-        depsEntrypoint = `${depsEntrypoint}.${extension}`;
-    }
-
-    const addDebug = <string> prompt("Add debug configuration? (y/n)", defaults.debug);
-    
-    if (addDebug !== 'y' && addDebug !== 'Y') {
-        defaults.debug = addDebug;
-    }
-
-    await checkTemplateArg();
-    await addContents(entrypoint, depsEntrypoint);
-}
-
-async function checkTemplateArg() { 
-    if (args.template) {
-        const template = await import(`./templates/${args.template}.${defaults.extension}`);
+async function fetchTemplate(t: string | undefined) { 
+    if (t) {
+        const template = await import(`./templates/${t}.${defaults.extension}`);
         defaults.module = encoder.encode(template.entrypoint.replace(/\$\{extension\}/g, defaults.extension));
         defaults.depsModule = encoder.encode(template.deps);
     }
--- utils.ts
+++ utils.ts
@@ -1,9 +1,8 @@
-import { args } from './parser.ts';
 
-async function writeFileOrWarn(path: string | URL, data: Uint8Array, options?: Deno.WriteFileOptions | undefined)
+async function writeFileOrWarn(path: string | URL, data: Uint8Array, force: boolean = false)
     : Promise<void> {
-    if (args.force) {
-        return await Deno.writeFile(path, data, options);
+    if (force) {
+        return await Deno.writeFile(path, data);
     }
 
     try {
@@ -12,21 +11,21 @@ async function writeFileOrWarn(path: string | URL, data: Uint8Array, options?: D
             console.warn(`Warning: file ${path} already exists. Pass --force to overwrite`);
         }
     } catch(_error) {
-        await Deno.writeFile(path, data, options);
+        await Deno.writeFile(path, data);
     }
 }
 
-async function mkdirOrWarn(path: string | URL, options?: Deno.WriteFileOptions | undefined)
+async function mkdirOrWarn(path: string | URL, force: boolean = false)
     : Promise<void> {
     try {
-        await Deno.mkdir(path, options);
+        await Deno.mkdir(path);
     } catch(_error) {
-        if (args.force) return;
+        if (force) return;
         console.warn(`Warning: Directory ${path} already exists.`);
     }
 }
 
- function hasFileExtension(filename: string, extension: string): boolean {
+function hasFileExtension(filename: string, extension: string): boolean {
     return new RegExp(`^[_A-Za-z-]+\.${extension}$`).test(filename);
 }
 
