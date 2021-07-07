37c34ccc0872ecdc45dc093e732035bfa30c0a75
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-07-07 21:44:05 +0200

    refactor: move and remove some code

--- commands/cli.ts
+++ commands/cli.ts
@@ -1,16 +1,17 @@
 import { Command, EnumType } from "../deps.ts";
-import { act } from "./act.ts";
+import { act, chooseTemplate } from "./act.ts";
 
-export const template = new EnumType(["cliffy"]);
+export const cliTemplate = new EnumType(["cliffy"]);
 
 export const cli = new Command()
     .name("cli")
     .description("Initialize a Deno Command Line Interface (CLI).")
-    .type("template", template)
-    .option<{ template: typeof template }>(
+    .type("template", cliTemplate)
+    .option<{ template: typeof cliTemplate }>(
     "-t, --template [method:template]",
     "Initialize the CLI from a template."
     )
     .action(async ({ editor, force, name, template }) => {
-        await act(editor,force, name, template, template.values());
+        const choice = await chooseTemplate(template, cliTemplate.values());
+        await act(editor, force, name, choice ?? template);
     });
