ee72f16bd535298e15f6e817a93278057fb80a81
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-07-11 11:32:20 +0200

    chore: remove unused dependency

--- deps.ts
+++ deps.ts
@@ -3,5 +3,4 @@ export {
   EnumType,
 } from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
 export { Select } from "https://deno.land/x/cliffy@v0.19.2/prompt/select.ts";
-export { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
 export { Rhum } from "https://deno.land/x/rhum@v1.1.10/mod.ts";
