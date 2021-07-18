526cde184f8066af045a58b049a4135cb35eb810
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2021-07-18 13:35:55 +0200

    chore: bump cliffy to 0.19.3

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   Command,
   EnumType,
-} from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
-export { Select } from "https://deno.land/x/cliffy@v0.19.2/prompt/select.ts";
+} from "https://deno.land/x/cliffy@v0.19.3/command/mod.ts";
+export { Select } from "https://deno.land/x/cliffy@v0.19.3/prompt/select.ts";
