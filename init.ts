2b416011701f89fb126b31df82ced09de6167b4c
Geert-Jan Zwiers <34610306+GJZwiers@users.noreply.github.com>
2021-10-19 20:23:29 +0200

    refactor: move prompt function to separate file (#34)
    * refactor: move prompt function to separate file
    
    * chore: run deno fmt
    
    * chore: re-enable ci setting

--- init.ts
+++ init.ts
@@ -1,7 +1,7 @@
 import { Command } from "./deps.ts";
 import { act } from "./act.ts";
 import { settings } from "./settings.ts";
-import { hasFileExtension } from "./utils.ts";
+import { ask } from "./ask.ts";
 
 /**
  * Main CLI command, as of right now the CLI does not have sub-commands.
@@ -74,56 +74,3 @@ await new Command()
     }
   })
   .parse(Deno.args);
-
-function ask() {
-  const ts = prompt("Use TypeScript?", "y");
-
-  settings.extension = (ts === "y" || ts === "Y") ? "ts" : "js";
-
-  settings.entrypoint = prompt(
-    `Set entrypoint:`,
-    `mod.${settings.extension}`,
-  ) ?? "mod";
-
-  settings.depsEntrypoint = prompt(
-    "Set dependency entrypoint:",
-    `deps.${settings.extension}`,
-  ) ?? "deps";
-
-  settings.devDepsEntrypoint = prompt(
-    "Set dev dependency entrypoint:",
-    `dev_deps.${settings.extension}`,
-  ) ?? "dev_deps";
-
-  if (!settings.map) {
-    const importMap = prompt(
-      "Add import map?",
-      "n",
-    );
-    settings.map = (importMap === "y" || importMap === "Y") ? true : false;
-  }
-
-  if (!settings.config) {
-    const config = prompt(
-      "Add Deno configuration file?",
-      "n",
-    );
-    settings.config = (config === "y" || config === "Y") ? true : false;
-  }
-
-  if (hasFileExtension(settings.entrypoint, settings.extension) === false) {
-    settings.entrypoint = `${settings.entrypoint}.${settings.extension}`;
-  }
-
-  if (hasFileExtension(settings.depsEntrypoint, settings.extension) === false) {
-    settings.depsEntrypoint =
-      `${settings.depsEntrypoint}.${settings.extension}`;
-  }
-
-  if (
-    hasFileExtension(settings.devDepsEntrypoint, settings.extension) === false
-  ) {
-    settings.devDepsEntrypoint =
-      `${settings.devDepsEntrypoint}.${settings.extension}`;
-  }
-}
