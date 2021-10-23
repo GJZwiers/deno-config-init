db8cfcea2152462261d90fd98273fc4fce1f23d6
Geert-Jan Zwiers <34610306+GJZwiers@users.noreply.github.com>
2021-10-23 22:03:54 +0200

    refactor: switch to built-in test step api (#42)
    * refactor: switch to built-in test step api
    
    * chore(deps): remove Rhum dev dependency
    
    * test(ci): use deno v1.1.5 explicitly
    
    * test(ci): use setup v1.1
    
    * test: use latest patch version
    
    * test: add unstable flag

--- act.test.ts
+++ act.test.ts
@@ -1,139 +1,146 @@
-import { assertThrowsAsync, Rhum } from "./dev_deps.ts";
+import { assert, assertEquals, assertThrowsAsync } from "./dev_deps.ts";
 import { act, runCommand } from "./act.ts";
 import { defaults } from "./settings.ts";
 
-Rhum.testPlan("act.test.ts", () => {
-  const settingsMock = self.structuredClone(defaults);
+Deno.test("runCommand()", async (t) => {
+  await t.step(
+    "should return true when a command's exit code is 0",
+    async () => {
+      const cmd = Deno.run({
+        cmd: ["git", "init"],
+      });
 
-  Rhum.testSuite("runCommand()", () => {
-    Rhum.testCase(
-      "should return true when a command's exit code is 0",
-      async () => {
-        const cmd = Deno.run({
-          cmd: ["git", "init"],
-        });
+      assertEquals(await runCommand(cmd), true);
+    },
+  );
 
-        Rhum.asserts.assertEquals(await runCommand(cmd), true);
-      },
-    );
+  await t.step(
+    "should return false when a command's exit code is greater than 0",
+    async () => {
+      const cmd = Deno.run({
+        cmd: ["git", "checkout", "foo"],
+      });
 
-    Rhum.testCase(
-      "should return false when a command's exit code is greater than 0",
-      async () => {
-        const cmd = Deno.run({
-          cmd: ["git", "checkout", "foo"],
-        });
-
-        Rhum.asserts.assertEquals(await runCommand(cmd), false);
-      },
-    );
-  });
-
-  Rhum.testSuite("act()", () => {
-    Rhum.beforeAll(() => {
-      settingsMock.git = false;
-      settingsMock.path = "test_directory_act";
-    });
-
-    Rhum.beforeEach(async () => {
-      await Deno.mkdir(settingsMock.path, { recursive: true });
-    });
-
-    Rhum.afterEach(async () => {
-      await Deno.remove(settingsMock.path, { recursive: true });
-    });
-
-    Rhum.testCase(
-      "should init git if settings.git is true",
-      async () => {
-        settingsMock.git = true;
-
-        await act(settingsMock);
-
-        Rhum.asserts.assertExists(
-          `${settingsMock.path}/.git`,
-        );
-
-        settingsMock.git = false;
-      },
-    );
-
-    Rhum.testCase(
-      "should create import_map.json if setting.map is true",
-      async () => {
-        settingsMock.map = true;
-
-        await act(settingsMock);
-
-        const mapFile = await Deno.readFile(
-          `${settingsMock.path}/import_map.json`,
-        );
-
-        Rhum.asserts.assert(mapFile);
-      },
-    );
-
-    Rhum.testCase(
-      "should create deno.json if setting.config is true",
-      async () => {
-        settingsMock.config = true;
-        settingsMock.map = false;
-        settingsMock.git = false;
-
-        await act(settingsMock);
-
-        const configFile = await Deno.readFile(
-          `${settingsMock.path}/deno.json`,
-        );
-
-        Rhum.asserts.assert(configFile);
-      },
-    );
-
-    Rhum.testCase(
-      "should create .test file for module entrypoint if settings.testdriven is true",
-      async () => {
-        settingsMock.testdriven = true;
-        settingsMock.map = false;
-        settingsMock.git = false;
-
-        await act(settingsMock);
-
-        const mapFile = await Deno.readFile(
-          `${settingsMock.path}/mod.test.ts`,
-        );
-
-        Rhum.asserts.assert(mapFile);
-      },
-    );
-
-    Rhum.testCase(
-      "should only create configuration file(s) and no module entrypoints if settings.configOnly is true",
-      async () => {
-        settingsMock.configOnly = true;
-        settingsMock.map = false;
-        settingsMock.git = false;
-
-        await act(settingsMock);
-
-        const configFile = await Deno.readFile(
-          `${settingsMock.path}/deno.json`,
-        );
-
-        Rhum.asserts.assert(configFile);
-
-        await assertThrowsAsync(async () => {
-          await Deno.readFile(`${settingsMock.path}/mod.ts`);
-        });
-        await assertThrowsAsync(async () => {
-          await Deno.readFile(`${settingsMock.path}/deps.ts`);
-        });
-        await assertThrowsAsync(async () => {
-          await Deno.readFile(`${settingsMock.path}/dev_deps.ts`);
-        });
-      },
-    );
-  });
+      assertEquals(await runCommand(cmd), false);
+    },
+  );
 });
 
-Rhum.run();
+const settingsMock = self.structuredClone(defaults);
+
+Deno.test("act()", async (t) => {
+  settingsMock.path = "test_directory_act";
+
+  const beforeEach = async () => {
+    await Deno.mkdir(settingsMock.path, { recursive: true });
+  };
+
+  const afterEach = async () => {
+    await Deno.remove(settingsMock.path, { recursive: true });
+    settingsMock.config = false;
+    settingsMock.configOnly = false;
+    settingsMock.git = false;
+    settingsMock.map = false;
+  };
+
+  await t.step(
+    "should init git if settings.git is true",
+    async () => {
+      await beforeEach();
+
+      settingsMock.git = true;
+
+      await act(settingsMock);
+
+      assert(`${settingsMock.path}/.git`);
+
+      await afterEach();
+    },
+  );
+
+  await t.step(
+    "should create import_map.json if setting.map is true",
+    async () => {
+      await beforeEach();
+
+      settingsMock.map = true;
+
+      await act(settingsMock);
+
+      const mapFile = await Deno.readFile(
+        `${settingsMock.path}/import_map.json`,
+      );
+
+      assert(mapFile);
+
+      await afterEach();
+    },
+  );
+
+  await t.step(
+    "should create deno.json if setting.config is true",
+    async () => {
+      await beforeEach();
+
+      settingsMock.config = true;
+
+      await act(settingsMock);
+
+      const configFile = await Deno.readFile(
+        `${settingsMock.path}/deno.json`,
+      );
+
+      assert(configFile);
+
+      await afterEach();
+    },
+  );
+
+  await t.step(
+    "should create .test file for module entrypoint if settings.testdriven is true",
+    async () => {
+      await beforeEach();
+
+      settingsMock.testdriven = true;
+
+      await act(settingsMock);
+
+      const mapFile = await Deno.readFile(
+        `${settingsMock.path}/mod.test.ts`,
+      );
+
+      assert(mapFile);
+
+      await afterEach();
+    },
+  );
+
+  await t.step(
+    "should only create configuration file(s) and no module entrypoints if settings.configOnly is true",
+    async () => {
+      await beforeEach();
+
+      settingsMock.configOnly = true;
+
+      await act(settingsMock);
+
+      const configFile = await Deno.readFile(
+        `${settingsMock.path}/deno.json`,
+      );
+
+      assert(configFile);
+
+      await assertThrowsAsync(async () => {
+        await Deno.readFile(`${settingsMock.path}/mod.ts`);
+      });
+      await assertThrowsAsync(async () => {
+        await Deno.readFile(`${settingsMock.path}/deps.ts`);
+      });
+      await assertThrowsAsync(async () => {
+        await Deno.readFile(`${settingsMock.path}/dev_deps.ts`);
+      });
+
+      await afterEach();
+    },
+  );
+});
