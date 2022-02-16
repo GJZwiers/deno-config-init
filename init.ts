3aff0616b6d7681588b0db13167183649079bd4e
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-02-16 20:08:13 +0100

    chore: patch version

--- init.ts
+++ init.ts
@@ -4,7 +4,7 @@ import { ask } from "./ask.ts";
 
 await new Command()
   .name("deno-init")
-  .version("v2.1.0")
+  .version("v2.1.1")
   .description("Generate a Deno configuration file.")
   .help({
     colors: (Deno.build.os === "windows") ? false : true,
