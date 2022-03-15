506421e9381c5f6f0c6b9bb1a27a035eec9fdbfa
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-03-15 20:08:50 +0100

    refactor round 1

--- scheme.ts
+++ scheme.ts
@@ -6,27 +6,26 @@ import json from "https://deno.land/x/deno@v1.19.3/cli/schemas/config-file.v1.js
 const anyJson = json as any;
 const jsonc: any = {};
 
-for (const prop in anyJson.properties) {
-  jsonc[prop] = {};
-  const options = anyJson.properties[prop].properties;
-  if (prop == "compilerOptions") {
+if ("properties" in anyJson) {
+  for (const prop in anyJson.properties) {
+    jsonc[prop] = {};
+    const options = anyJson.properties[prop].properties;
     for (const option in options) {
-      jsonc[prop][option] = [
-        options[option].type ?? "type",
-        options[option].default,
-        options[option].description,
-      ].join("|");
-    }
-  } else {
-    for (const option in options) {
-      jsonc[prop][option] = {};
-      for (const opt in options[option].properties) {
-        const o = options[option].properties[opt];
-
-        jsonc[prop][option][opt] = [
-          o.type ?? "type",
-          o.default ?? "none",
-          o.description,
+      if ("properties" in options[option]) {
+        jsonc[prop][option] = {};
+        for (const opt in options[option].properties) {
+          const o = options[option].properties[opt];
+          jsonc[prop][option][opt] = [
+            o.type ?? "type",
+            o.default ?? "none",
+            o.description,
+          ].join("|");
+        }
+      } else {
+        jsonc[prop][option] = [
+          options[option].type ?? "type",
+          options[option].default ?? "none",
+          options[option].description,
         ].join("|");
       }
     }
@@ -35,45 +34,53 @@ for (const prop in anyJson.properties) {
 
 const jsoncString = JSON.stringify(jsonc, null, 2);
 
+//console.log(jsonc)
+
 const final = jsoncString.replace(
   /^(\s+".+?": )"(.+?)\|(.+?)\|(.+?)",?$/gm,
   function (
     _full_match,
-    prop,
+    property,
     type,
     defaultValue,
-    desc,
+    description,
   ) {
-    if (defaultValue === "none") {
-      return prop.replace(' "', ' // "') + "[] // " + desc;
+    let value;
+    if (type === "string") {
+      value = `"${defaultValue}"`;
+    } else if (type === "array" && defaultValue === "none") {
+      value = "[]";
+    } else if (type === "array") {
+      value = `[ "${defaultValue}" ]`;
+    } else {
+      value = defaultValue;
     }
-    if (type === "array") {
-      return prop.replace(' "', ' // "') + '[ "' + defaultValue + '" ]' +
-        " // " + desc;
-    }
-
-    return prop.replace(' "', ' // "') + defaultValue + " // " + desc;
+    
+    // add property as: // "<key>": <value> /* <description> */
+    return property.replace(/^(\s*?)(?=")/, '$1// ') + value + " /* " + description + " */";
   },
 );
 
+const lines = final.split("\n");
 let highest = 0;
-final.split("\n").forEach((line) => {
-  const matches = [...line.matchAll(/\/\//g)];
 
-  if (matches.length === 0 || !matches[1].index) return;
-  if (matches[1].index > highest) return highest = matches[1].index;
+lines.forEach((line) => {
+  const matches = [...line.matchAll(/\/\*/g)];
+
+  if (matches.length === 0 || !matches[0].index) return;
+  if (matches[0].index > highest) return highest = matches[0].index;
 });
 
-const finalFinal = final.split("\n").map((line) => {
-  const matches = [...line.matchAll(/(?<!^\s+)\/\//g)];
+const configFileContents = lines.map((line) => {
+  const matches = [...line.matchAll(/\/\*/g)];
 
   if (matches.length === 0 || !matches[0]) return line;
 
-  return line.replace(/(?<!^\s+)\/\//, function (match) {
+  return line.replace(/\/\*/, function (match) {
     const diff = highest - (matches[0].index || 0);
 
     return " ".repeat(diff) + match;
   });
-});
+}).join("\n");
 
-await Deno.writeTextFile("deno.jsonc", finalFinal.join("\n"));
+await Deno.writeTextFile("deno.jsonc", configFileContents);
