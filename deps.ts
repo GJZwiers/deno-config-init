c9c9e21268f961de48d5a0b5cf422bc1b3ca7b2f
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-03-17 20:35:03 +0100

    chore: upgrade to deno 1.20 config file schema

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { Command } from "https://deno.land/x/cliffy@v0.20.1/command/mod.ts";
 export {
   default as schema,
-} from "https://deno.land/x/deno@v1.19.3/cli/schemas/config-file.v1.json" assert {
+} from "https://deno.land/x/deno@v1.20.1/cli/schemas/config-file.v1.json" assert {
   type: "json",
 };
