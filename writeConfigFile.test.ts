57881e867c3cf4abbdde1653865bd3086c2a549f
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-07-21 19:40:43 +0200

    feat: support deno test configs (#117)
    * feat: support deno test configs
    
    * update lockfile
    
    * update docs
    
    * fmt

--- writeConfigFile.test.ts
+++ writeConfigFile.test.ts
@@ -222,4 +222,22 @@ Deno.test("writeConfigFile", async (context) => {
       assert(bytes.length > 0);
     },
   });
+
+  await test({
+    name: "create test",
+    fn: async () => {
+      testOpts.test = true;
+
+      const output = await inputHandler(testOpts);
+
+      const bytes = await Deno.readFile(
+        `${testOpts.name}`,
+      );
+
+      assert(bytes.length > 0);
+      assertEquals(testOpts.name, "deno.json");
+
+      await assertSnapshot(context, output.split("\n"));
+    },
+  });
 });
