7cacaeb92042d689b9811a2face4ce3bcb313d2b
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-08-04 19:19:39 +0200

    chore: update dependencies

--- processOptions.ts
+++ processOptions.ts
@@ -2,6 +2,17 @@ import { promptUserInputs } from "./promptUserInputs.ts";
 import { defaultOpts, type Options } from "./writeConfigFile.ts";
 
 export function process(opts: Options): Options {
+  // TODO: investigate using the json schema to check this instead
+  if (opts.map && !isSupported("map", Deno.version.deno)) {
+    throw new Error("The option '--map' requires Deno v1.20 or higher.");
+  }
+  if (opts.task && !isSupported("task", Deno.version.deno)) {
+    throw new Error("The option '--task' requires Deno v1.20 or higher.");
+  }
+  if (opts.test && !isSupported("test", Deno.version.deno)) {
+    throw new Error("The option '--test' requires Deno v1.24 or higher.");
+  }
+
   if (opts.yes) {
     if (
       opts.tsconfig || opts.fmt || opts.lint || opts.task || opts.map ||
@@ -20,4 +31,35 @@ export function process(opts: Options): Options {
     const choices = promptUserInputs({ testmode: false });
     return { ...opts, ...choices };
   }
+}
+
+type FeatureVersions = {
+  [key: string]: {
+    major: number;
+    minor: number;
+  };
+};
+
+const featureVersionReqs: FeatureVersions = {
+  map: {
+    major: 1,
+    minor: 20,
+  },
+  task: {
+    major: 1,
+    minor: 20,
+  },
+  test: {
+    major: 1,
+    minor: 24,
+  },
+};
+
+function isSupported(feature: string, v: string): boolean {
+  const components = v.split(".");
+  const major = parseInt(components[0]);
+  const minor = parseInt(components[1]);
+
+  return major >= featureVersionReqs[feature].major &&
+    minor >= featureVersionReqs[feature].minor;
 }
