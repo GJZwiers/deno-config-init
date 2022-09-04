434429ac1b1a56a105353c2a09c02c6c3a3a2e56
GitHub <noreply@github.com>
2022-09-04 19:50:17 +0000

    chore(deps): update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { Command } from "https://deno.land/x/cliffy@v0.24.3/command/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.25.0/command/mod.ts";
 export {
   default as schema,
 } from "https://deno.land/x/deno@v1.25.1/cli/schemas/config-file.v1.json" assert {
