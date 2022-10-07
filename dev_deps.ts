dafea563c7057c68b8448b70cd74488c956b1ca6
GitHub <noreply@github.com>
2022-10-07 19:51:34 +0000

    chore(deps): update dependencies

--- dev_deps.ts
+++ dev_deps.ts
@@ -3,7 +3,7 @@ export {
   assertEquals,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.157.0/testing/asserts.ts";
+} from "https://deno.land/std@0.159.0/testing/asserts.ts";
 export {
   assertSnapshot,
-} from "https://deno.land/std@0.157.0/testing/snapshot.ts";
+} from "https://deno.land/std@0.159.0/testing/snapshot.ts";
