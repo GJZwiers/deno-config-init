7993e34dc4bcd3eb495c782aff092d115fb826d4
denopendabot <denopendabot@gmail.com>
2022-10-22 22:16:28 +0200

    build(deps): bump deno.land/std from 0.159.0 to 0.160.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -6,4 +6,4 @@ export {
 } from "https://deno.land/std@0.159.0/testing/asserts.ts";
 export {
   assertSnapshot,
-} from "https://deno.land/std@0.159.0/testing/snapshot.ts";
+} from "https://deno.land/std@0.160.0/testing/snapshot.ts";
