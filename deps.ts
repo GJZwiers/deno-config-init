b0eadd333b42e476c55ae72531af8f6d68a8b690
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-12-15 16:29:25 +0100

    chore: update snapshot for v1.29

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
 export {
   default as schema,
-} from "https://deno.land/x/deno@v1.28.2/cli/schemas/config-file.v1.json" assert {
+} from "https://deno.land/x/deno@v1.29.0/cli/schemas/config-file.v1.json" assert {
   type: "json",
 };
