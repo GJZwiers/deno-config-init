f9c55aa5a9835c6aec56d424309c5918cc63ada4
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-12-18 21:16:59 +0100

    feat: support custom lock file name

--- init.ts
+++ init.ts
@@ -11,55 +11,59 @@ await new Command()
   })
   .option(
     "-a, --fill [fill:boolean]",
-    "Create the config file as .jsonc with the possible options listed as comments.",
+    "Create the config file as .jsonc with the possible options listed as comments",
   )
   .option(
     "-b, --bench [bench:boolean]",
-    "Set up config for deno bench only. Requires Deno 1.29 or higher",
+    "Add config for deno bench only. Requires Deno 1.29 or higher",
   )
   .option(
     "-c, --jsonc [jsonc:boolean]",
-    "Create the config file as .jsonc. Alias for --fill.",
+    "Create the config file as .jsonc. Alias for --fill",
   )
   .option(
     "-f, --force [force:boolean]",
-    "Allow overwriting an existing config file.",
+    "Allow overwriting an existing config file",
   )
   .option(
     "-n, --name <name:string>",
-    "The name of the config file.",
+    "Set a custom name for the config file",
   )
   .option(
     "-m, --fmt [fmt:boolean]",
-    "Set up config for deno fmt only.",
+    "Add config for deno fmt only",
   )
   .option(
     "-p, --map [map:boolean]",
-    "Set up config for an import map. Requires Deno 1.20 or higher",
+    "Add config for an import map. Requires Deno 1.20 or higher",
   )
   .option(
     "-l, --lint [lint:boolean]",
-    "Set up config for deno lint only.",
+    "Add config for deno lint only",
   )
   .option(
-    "-o, --lock <lock:string>",
-    "The name of the lock file.",
+    "-o, --lock [lock:boolean]",
+    "Enable or disable lock file generation",
+  )
+  .option(
+    "-i, --lockfile <lockfile:string>",
+    "Set a custom name for the lock file",
   )
   .option(
     "-k, --task [task:boolean]",
-    "Set up config for deno tasks only. Requires Deno 1.20 or higher",
+    "Add config for deno tasks only. Requires Deno 1.20 or higher",
   )
   .option(
     "-s, --test [test:boolean]",
-    "Set up config for deno test only. Requires Deno 1.24 or higher",
+    "Add config for deno test only. Requires Deno 1.24 or higher",
   )
   .option(
     "-t, --tsconfig [tsconfig:boolean]",
-    "Set up config for typescript compiler options only.",
+    "Add config for typescript compiler options only.",
   )
   .option(
     "-y, --yes [yes:boolean]",
-    "Skip the prompts and use all defaults.",
+    "Skip the prompts and use all defaults",
   )
   // deno-lint-ignore no-explicit-any
   .action(async (options: any) => {
--- processOptions.ts
+++ processOptions.ts
@@ -4,28 +4,40 @@ import { defaultOpts, type Options } from "./writeConfigFile.ts";
 export function process(opts: Options): Options {
   // TODO: investigate using the json schema to check this instead
   if (opts.map && !isSupported("map", Deno.version.deno)) {
-    throw new Error("The option '--map' requires Deno v1.20 or higher.");
+    throw new Error("The option '--map' requires Deno v1.20 or higher");
   }
   if (opts.task && !isSupported("task", Deno.version.deno)) {
-    throw new Error("The option '--task' requires Deno v1.20 or higher.");
+    throw new Error("The option '--task' requires Deno v1.20 or higher");
   }
   if (opts.test && !isSupported("test", Deno.version.deno)) {
-    throw new Error("The option '--test' requires Deno v1.24 or higher.");
+    throw new Error("The option '--test' requires Deno v1.24 or higher");
+  }
+  if (opts.lock && !isSupported("test", Deno.version.deno)) {
+    throw new Error("The option '--lock' requires Deno v1.29 or higher");
+  }
+  if (opts.lockfile && !isSupported("test", Deno.version.deno)) {
+    throw new Error("The option '--lockfile' requires Deno v1.29 or higher");
+  }
+
+  if (opts.lock && opts.lockfile) {
+    throw new Error(
+      "'lock' and 'lockfile' cannot be used simultaneously. Use 'lockfile' to enable and set a custom lock file name, and use 'lock' to enable/disable deno.lock generation. Setting 'lockfile' implies 'lock': true",
+    );
   }
 
   if (opts.yes) {
     if (
       opts.tsconfig || opts.fmt || opts.lint || opts.task || opts.map ||
-      opts.test || opts.bench || opts.lock
+      opts.test || opts.bench || opts.lock || opts.lockfile
     ) {
-      throw new Error("--yes cannot be used together with other options.");
+      throw new Error("--yes cannot be used with other options");
     }
   }
-
+  
   if (
     opts.yes || opts.fill || opts.fmt || opts.lint ||
     opts.tsconfig || opts.jsonc || opts.task || opts.map || opts.test ||
-    opts.bench || opts.lock
+    opts.bench || opts.lock || opts.lockfile
   ) {
     return { ...defaultOpts, ...opts };
   } else {
@@ -54,6 +66,14 @@ const featureVersionReqs: FeatureVersions = {
     major: 1,
     minor: 24,
   },
+  lock: {
+    major: 1,
+    minor: 29,
+  },
+  lockfile: {
+    major: 1,
+    minor: 29,
+  }
 };
 
 function isSupported(feature: string, v: string): boolean {
