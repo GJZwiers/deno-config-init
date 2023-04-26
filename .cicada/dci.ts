a33c6098f597b3adbd0f0dcbc0d2930f7bbb0386
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2023-04-26 09:59:23 +0200

    feat: add cicada ci

---
+++ .cicada/dci.ts
@@ -0,0 +1,25 @@
+import { Job, Pipeline } from "https://deno.land/x/cicada@v0.1.41/mod.ts";
+
+const job = new Job({
+  name: "My First Job",
+  image: "ubuntu:22.04",
+  steps: [
+    {
+      name: "Run bash",
+      run: "echo hello from bash!",
+    },
+    {
+      name: "Run deno/typescript",
+      run: () => {
+        console.log("Hello from deno typescript");
+        console.log("I can read the local file system");
+        console.log("e.g. here are all the directories in my project");
+        console.log(Array.from(Deno.readDirSync("/app")));
+        console.log("Or I can see my environment variables");
+        console.log(Deno.env.toObject());
+      },
+    },
+  ],
+});
+
+export default new Pipeline([job]);
